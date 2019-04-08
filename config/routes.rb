c38e94a588aeb42d88f76c0a398080ff672085cc
Sofia <jonssonsofia93@gmail.com>
2019-04-08 13:54:33 -0700

    Added Todo Lists & Todo Items

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :todo_lists do
+    resources :todo_items
+  end
+  #this is adding a block within todo_lists
+
+  root "todo_lists#index"
 end
